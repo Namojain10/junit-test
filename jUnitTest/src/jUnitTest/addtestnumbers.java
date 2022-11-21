0f0f703520792b608e1815514a11fbdf574345a4
namo <namojain.2003@gmail.com>
2022-11-21 15:20:41 +0530

    namo

---
+++ jUnitTest/src/jUnitTest/addtestnumbers.java
@@ -0,0 +1,15 @@
+package jUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class addtestnumbers{
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
---
+++ jUnitTest/src/jUnitTest/testaddnumber.java
@@ -0,0 +1,14 @@
+package jUnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testaddnumber {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
